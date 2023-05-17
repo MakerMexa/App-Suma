84ec40a43ad8e301d26888ea7bd9eeca027e8643
makerino <joselomelim29@gmail.com>
2023-05-17 09:59:41 -0600

    app con js que suma 2 numeros v1

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('aplicacion calculadora');
+function sumar(){
+    const forma= document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La Operacion no Incluye Numeros';    
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`resultado: ${resultado}`);
+}
